c8761b2c702647448df681a641fb48992cd7996f
mediaslug <github@mediaslug.com>
2016-03-31 17:00:13 -0500

    concatenated javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp'),                   // assign all methods an properties of gulp to this variable
     gutil = require('gulp-util'),             // also create var for gulp-util
-    coffee = require('gulp-coffee'); 
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat'); 
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+
+var jsSources = ['components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+];
 
 
 // create a task by using the task method
@@ -15,4 +22,10 @@ gulp.task('coffee', function() { // define the coffee task
         .pipe(coffee({bare:true})
             .on('error', gutil.log))
         .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function() {
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'));
 });
