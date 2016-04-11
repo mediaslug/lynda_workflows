bc953f742440180ea49d5b557ec605cc5a188ec3
mediaslug <github@mediaslug.com>
2016-04-11 16:01:19 -0500

    minified html files

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulp = require('gulp'),                   // assign all methods an propertie
     compass = require('gulp-compass'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
-
+    minifyHTML = require('gulp-minify-html'),
     connect = require('gulp-connect'); 
 
 var env,
@@ -77,7 +77,7 @@ gulp.task('watch', function() {
     gulp.watch(coffeeSources, ['coffee']); 
     gulp.watch(jsSources, ['js']); 
     gulp.watch('components/sass/*.scss', ['compass']); 
-    gulp.watch(htmlSources, ['html']);
+    gulp.watch('builds/development/*.html', ['html']);
     gulp.watch(jsonSources, ['json']);
 
 });
@@ -91,8 +91,10 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function(){
-   gulp.src(htmlSources)
-   .pipe(connect.reload());
+   gulp.src('builds/development/*.html')
+       .pipe(gulpif(env==='production', minifyHTML()))
+       .pipe(gulpif(env==='production', gulp.dest(outputDir)))
+       .pipe(connect.reload());
 });
 
 gulp.task('json', function(){
